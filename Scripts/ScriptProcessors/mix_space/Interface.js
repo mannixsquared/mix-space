fafb3cb454d61f6acf2f41204a4e447b21067126
John Mannix <1419878+johnmannix@users.noreply.github.com>
2024-06-05 09:31:36 +0100

    Initial commit

---
+++ Scripts/ScriptProcessors/mix_space/Interface.js
@@ -0,0 +1,73 @@
+Content.makeFrontInterface(730, 450);
+
+Content.getComponent("Threshold").setControlCallback(onThresholdControl);
+Content.getComponent("Ratio").setControlCallback(onRatioControl);
+Content.getComponent("Attack").setControlCallback(onAttackControl);
+Content.getComponent("Release").setControlCallback(onReleaseControl);
+Content.getComponent("Smoothing").setControlCallback(onSmoothingControl);
+
+const var ThresholdValue = Content.getComponent("ThresholdValue");
+const var RatioValue = Content.getComponent("RatioValue");
+const var AttackValue = Content.getComponent("AttackValue");
+const var ReleaseValue = Content.getComponent("ReleaseValue");
+const var SmoothingValue = Content.getComponent("SmoothingValue");
+const var DynamicEQ = Synth.getEffect("DynamicEQ");
+
+inline function onThresholdControl(component, value)
+{	
+	local v = Engine.doubleToString(value, 2) + "db";
+	DynamicEQ.setAttribute(0, value);
+	ThresholdValue.set("text", v);
+};
+
+inline function onRatioControl(component, value)
+{
+	local v = Engine.doubleToString(value, 1);
+	DynamicEQ.setAttribute(1, value);
+	RatioValue.set("text", v);
+};
+
+
+inline function onAttackControl(component, value)
+{
+	local v = Engine.doubleToString(value, 1) + "ms";
+	DynamicEQ.setAttribute(2, value);
+	AttackValue.set("text", v);
+};
+
+
+inline function onReleaseControl(component, value)
+{
+	local v = Engine.doubleToString(value, 1) + "ms";
+	DynamicEQ.setAttribute(3, value);
+	ReleaseValue.set("text", v);
+};
+
+
+inline function onSmoothingControl(component, value)
+{
+	local v = Engine.doubleToString(value, 1);
+	DynamicEQ.setAttribute(4, value);
+	SmoothingValue.set("text", v);
+}
+function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
